92a9d117e61040a246c199b088153544b2af2722
yu256 <122450013+yu256@users.noreply.github.com>
2024-02-25 15:15:22 +0900

    PWA対応

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,46 @@
 import { defineConfig } from "vite";
 import { preact } from "@preact/preset-vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [
+		preact(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+			injectRegister: "auto",
+			manifest: {
+				name: "tetris_wasm",
+				short_name: "tetris",
+				description: "Tetris written in C++ and React",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+				],
+			},
+		}),
+	],
 	base: "/tetris_wasm/",
 });
